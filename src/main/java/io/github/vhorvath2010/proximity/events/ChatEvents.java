715eab1366968c320384937826e56a2fe5784a91
Vincent Horvath <vhorvath2010@gmail.com>
2021-01-24 18:33:58 -0500

    Added block muffling

--- src/main/java/io/github/vhorvath2010/proximity/events/ChatEvents.java
+++ src/main/java/io/github/vhorvath2010/proximity/events/ChatEvents.java
@@ -3,11 +3,14 @@ package io.github.vhorvath2010.proximity.events;
 import io.github.vhorvath2010.proximity.Proximity;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.util.BlockIterator;
+import org.bukkit.util.Vector;
 
 import java.util.*;
 
@@ -38,9 +41,25 @@ public class ChatEvents implements Listener {
     // This method will get the entities within a sphere of radius r around a location
     private static List<Entity> getEntitiesAroundPoint(Location location, double radius) {
         List<Entity> entities = new ArrayList<Entity>();
+        double muffleMag = Proximity.getPlugin().getConfig().getDouble("block_muffling");
         for (Entity entity : location.getWorld().getNearbyEntities(location, radius, radius, radius)) {
-            if (entity.getLocation().distanceSquared(location) <= radius * radius) {
+            // Account for block muffling
+            Vector from = location.toVector();
+            Vector to = entity.getLocation().toVector();
+            Vector dir = to.subtract(from);
+            BlockIterator iterator = new BlockIterator(location.getWorld(), from, dir, 1.0, (int) Math.ceil(entity.getLocation().distance(location)));
+            double muffle = 0;
+            while (iterator.hasNext()) {
+                Block block = iterator.next();
+                if (!block.isPassable()) {
+                    muffle += muffleMag;
+                }
+            }
+            // Apply block muffling to the radius
+            double effectiveRadius = radius - muffle;
+            if (entity.getLocation().distance(location) <= effectiveRadius) {
                 entities.add(entity);
+                System.out.println("A " + entity.getType() + " is in range");
             }
         }
         return entities;
