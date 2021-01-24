48c533ece3685aa00896ec2139cd06a52e9178fc
Vincent Horvath <vhorvath2010@gmail.com>
2021-01-23 23:20:00 -0500

    Improved nearby entities method

--- src/main/java/io/github/vhorvath2010/proxchat/events/ChatEvents.java
+++ src/main/java/io/github/vhorvath2010/proxchat/events/ChatEvents.java
@@ -39,28 +39,9 @@ public class ChatEvents implements Listener {
     // This method will get the entities within a sphere of radius r around a location
     private static List<Entity> getEntitiesAroundPoint(Location location, double radius) {
         List<Entity> entities = new ArrayList<Entity>();
-        World world = location.getWorld();
-
-        // To find chunks we use chunk coordinates (not block coordinates!)
-        int smallX = (int) Math.floor((location.getX() - radius) / 16.0D);
-        int bigX = (int) Math.floor((location.getX() + radius) / 16.0D);
-        int smallZ = (int) Math.floor((location.getZ() - radius) / 16.0D);
-        int bigZ = (int) Math.floor((location.getZ() + radius) / 16.0D);
-
-        for (int x = smallX; x <= bigX; x++) {
-            for (int z = smallZ; z <= bigZ; z++) {
-                if (world.isChunkLoaded(x, z)) {
-                    entities.addAll(Arrays.asList(world.getChunkAt(x, z).getEntities())); // Add all entities from this chunk to the list
-                }
-            }
-        }
-
-        // Remove the entities that are within the box above but not actually in the sphere we defined with the radius and location
-        // This code below could probably be replaced in Java 8 with a stream -> filter
-        Iterator<Entity> entityIterator = entities.iterator(); // Create an iterator so we can loop through the list while removing entries
-        while (entityIterator.hasNext()) {
-            if (entityIterator.next().getLocation().distanceSquared(location) > radius * radius) { // If the entity is outside of the sphere...
-                entityIterator.remove(); // Remove it
+        for (Entity entity : location.getWorld().getNearbyEntities(location, radius, radius, radius)) {
+            if (entity.getLocation().distanceSquared(location) <= radius * radius) {
+                entities.add(entity);
             }
         }
         return entities;
