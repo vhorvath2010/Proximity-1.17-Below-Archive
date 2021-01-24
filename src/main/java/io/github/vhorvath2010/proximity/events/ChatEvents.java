588bf86dc2be6b36e04f0b15275aa9a788b9137b
Vincent Horvath <vhorvath2010@gmail.com>
2021-01-24 18:48:26 -0500

    Large bug fixes

--- src/main/java/io/github/vhorvath2010/proximity/events/ChatEvents.java
+++ src/main/java/io/github/vhorvath2010/proximity/events/ChatEvents.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.BlockIterator;
 import org.bukkit.util.Vector;
 
@@ -25,7 +26,7 @@ public class ChatEvents implements Listener {
         Set<Player> recipients = event.getRecipients();
         recipients.clear();
         double radius = Proximity.getPlugin().getConfig().getDouble("chat_range");
-        for (Entity near : getEntitiesAroundPoint(event.getPlayer().getLocation(), radius)) {
+        for (Entity near : getEntitiesAroundPoint(event.getPlayer().getLocation(), radius, event.getPlayer())) {
             if (near instanceof Player) {
                 recipients.add((Player) near);
             }
@@ -36,32 +37,42 @@ public class ChatEvents implements Listener {
                 recipients.add(online);
             }
         }
+        // Add player
+        recipients.add(event.getPlayer());
     }
 
     // This method will get the entities within a sphere of radius r around a location
-    private static List<Entity> getEntitiesAroundPoint(Location location, double radius) {
+    private static List<Entity> getEntitiesAroundPoint(Location location, double radius, Player chatter) {
         List<Entity> entities = new ArrayList<Entity>();
         double muffleMag = Proximity.getPlugin().getConfig().getDouble("block_muffling");
-        for (Entity entity : location.getWorld().getNearbyEntities(location, radius, radius, radius)) {
-            // Account for block muffling
-            Vector from = location.toVector();
-            Vector to = entity.getLocation().toVector();
-            Vector dir = to.subtract(from);
-            BlockIterator iterator = new BlockIterator(location.getWorld(), from, dir, 1.0, (int) Math.ceil(entity.getLocation().distance(location)));
-            double muffle = 0;
-            while (iterator.hasNext()) {
-                Block block = iterator.next();
-                if (!block.isPassable()) {
-                    muffle += muffleMag;
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Entity entity : location.getWorld().getNearbyEntities(location, radius, radius, radius)) {
+                    // Stop if this is the player who sent the message
+                    if (entity.equals(chatter)) {
+                        continue;
+                    }
+                    // Account for block muffling
+                    Vector from = location.toVector();
+                    Vector to = entity.getLocation().toVector();
+                    Vector dir = to.subtract(from);
+                    BlockIterator iterator = new BlockIterator(location.getWorld(), from, dir, 1.0, (int) Math.ceil(entity.getLocation().distance(location)));
+                    double muffle = 0;
+                    while (iterator.hasNext()) {
+                        Block block = iterator.next();
+                        if (!block.isPassable()) {
+                            muffle += muffleMag;
+                        }
+                    }
+                    // Apply block muffling to the radius
+                    double effectiveRadius = radius - muffle;
+                    if (entity.getLocation().distance(location) <= effectiveRadius) {
+                        entities.add(entity);
+                    }
                 }
             }
-            // Apply block muffling to the radius
-            double effectiveRadius = radius - muffle;
-            if (entity.getLocation().distance(location) <= effectiveRadius) {
-                entities.add(entity);
-                System.out.println("A " + entity.getType() + " is in range");
-            }
-        }
+        }.runTaskLater(Proximity.getPlugin(), 0);
         return entities;
     }
 
